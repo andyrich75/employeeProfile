289b69377debe1cd9554f76a2400b42d380c7247
andyrich75 <arichard101@gmail.com>
2019-12-06 19:56:05 -0500

    added the const for the team

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const generateHTML = require("./output/generateHTML");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const team = [];
