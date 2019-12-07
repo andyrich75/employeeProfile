6a5c6ac095bb041ac117e83abf6200b2febf6f20
andyrich75 <arichard101@gmail.com>
2019-12-06 19:39:52 -0500

    added the require of the diff employee js files

---
+++ app.js
@@ -0,0 +1,5 @@
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const generateHTML = require("./output/generateHTML");
